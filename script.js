22bf8e004f42bdf980233da73e1b33c2646de4ce
brikmanis <97937078+brikmanis@users.noreply.github.com>
2023-03-28 08:36:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const plusins = document.querySelector('.js-plus');
 const inputins = document.querySelector('.js-input');
 
-const Add = () => {
-    
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
 }

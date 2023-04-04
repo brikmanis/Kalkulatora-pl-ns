4e9b295812372f2ca79567549bf514e3f3d30aca
brikmanis <97937078+brikmanis@users.noreply.github.com>
2023-04-04 08:18:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ const Multiply = (cipars1, cipars2) => {
 const Divide = (cipars1, cipars2) => {
     return cipars1 / cipars2
 }
+
+plusins.addEventListener('click', () => {
+    console.log("Mani nospieda")
+})
